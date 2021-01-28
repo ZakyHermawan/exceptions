78d444f681f9fa24f24d641d710e94f7fd7b6b35
ZakyHermawan <zaky.hermawan9615@gmail.com>
2021-01-28 22:31:03 +0700

    Add fail constructor

---
+++ fail_constructor.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class Member
+{
+public:
+    Member() {
+        cout << "Member created" << endl;
+    }
+
+    ~Member() {
+        cout << "Member destroyed" << endl;
+    }
+};
+
+class A
+{
+private:
+    int m_x;
+    Member m_member;
+
+public:
+    A(int x) : m_x{ 0 }
+    {
+        cout << "A is created" << endl;
+        if (x <= 0)
+            throw 1;
+    }
+
+    ~A() {
+        cout << "A is destroyed" << endl;
+    }
+};
+
+int main() {
+    try {
+        A a{ 0 };
+    }
+    catch (...) {
+        std::cout << "Error" << endl;
+    }
+
+}
